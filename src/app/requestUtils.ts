a2a8b95276654068ba057334789f3e3ca102b9c5
gonzalo <gonzalomedinamedina@gmail.com>
2023-08-06 13:49:16 +0200

    Created method to wrap request type.

--- src/app/requestUtils.ts
+++ src/app/requestUtils.ts
@@ -1,6 +1,6 @@
 export class RequestUtils
 {
-    static async postRequest(controller: string, body: string, callback: Function)
+    static async doRequest(type: string, controller: string, body: string, callback: Function)
     {
         const url = 'http://localhost:3000/api/' + controller;
 
@@ -10,7 +10,7 @@ export class RequestUtils
             {
                 'Content-Type': 'application/json'
             },
-            method: 'POST',
+            method: type,
             body: JSON.stringify(body)
         })
         .then(response => response.json())
@@ -18,9 +18,19 @@ export class RequestUtils
         {
             callback(data);
         })
-        .catch ( error => 
+        .catch(error => 
         {
             alert('Error: ' + error);
         });
+    }
+
+    static async postRequest(controller: string, body: string, callback: Function)
+    {
+        RequestUtils.doRequest('POST', controller, body, callback);
+    }
+    
+    static async getRequest(controller: string, body: string, callback: Function)
+    {
+        RequestUtils.doRequest('GET', controller, body, callback);
     }
 }
