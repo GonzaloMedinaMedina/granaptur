4f5401307b2499099a4cbb5e5ee4d19b6c764e9e
Gonzalo <gonzalomedinamedina@gmail.com>
2023-08-14 16:00:24 +0200

    Changing how ad info is retrieved.

--- src/app/editableAd/[editableAd]/page.tsx
+++ src/app/editableAd/[editableAd]/page.tsx
@@ -4,15 +4,13 @@ import { DataBaseService } from "@/app/databaseManager/databaseService";
 async function getAd(id:string)
 {
     const dtoName = 'ad';
-    const query = { id: id};
-    const dto = await DataBaseService.getDtoInfo(query, dtoName, true);
-
+    const dto = await DataBaseService.getDtoById(id, dtoName, true);
     return JSON.parse(dto);
 }
 
-export default async function EditableAdPage({params}: {params: {ad: string}})
+export default async function EditableAdPage({params}: {params: {editableAd: string}})
 {
-    const adInfo = await getAd(params.ad);
+    const adInfo = await getAd(params.editableAd);
 
     return ( <EditableAd adInfo={adInfo}></EditableAd>)
 }
