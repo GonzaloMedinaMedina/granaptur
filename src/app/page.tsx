981d86a351aa03dfbacde3e54b6e3a77882c0789
gonzalo <gonzalomedinamedina@gmail.com>
2023-10-15 13:06:31 +0200

    Removed unnused code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,13 @@ import Ad from "./components/ad";
 import { anuncios } from '@/app/anuncios';
 import { headerInfo } from "./headerInfo";
 import SlidePicture from "./components/pictureSlider/pictureSlider";
-import Pictures from "./components/pictures/Pictures";
 
 export default async function Home() 
 {
-  const ads = anuncios.map((adInfo: any) => {
+  const ads = anuncios.map((adInfo: any) => 
+  {
     return <li className="no-dot max-w-full my-16" key={adInfo.id}>
-      <Ad
-        picturesComponents={<Pictures/>}
-        adInfo={adInfo}
-      ></Ad>
+      <Ad adInfo={adInfo}></Ad>
     </li>
   })
 
@@ -19,7 +16,7 @@ export default async function Home()
     <>
       <div className="flex justify-center">
         <div className="shadow-container flex-col items-center border-adblueback bg-adblueback/50 m-10 p-5 w-[1000px] rounded-xl">
-          <SlidePicture width={'1000'} height={'600'} pictures={headerInfo.pictures} automaticSlider={true} text={headerInfo.title}></SlidePicture>
+            <SlidePicture width={'1000'} height={'600'} pictures={headerInfo.pictures} automaticSlider={true} text={headerInfo.title}></SlidePicture>
         </div>
     </div>
 
