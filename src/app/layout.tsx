8dc3ba7de8436fc7dc1029fa94deedf4e724e2e2
gonzalo <gonzalomedinamedina@gmail.com>
2023-10-17 09:49:15 +0200

    Using hexcode for bg color instead tailwind class.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className='bg-adblueback/10'>
+      <body style={{backgroundColor: '#0c69ba1a'}}>
         <NavBar/>
         <hr className='border-black/20'></hr>
-        <main className='bg-adblueback/10'>
+        <main style={{backgroundColor: '#0c69ba1a'}}>
           {children}
           <div className='h-[100px] bg-[#8c9bb3]'></div>
         </main>
